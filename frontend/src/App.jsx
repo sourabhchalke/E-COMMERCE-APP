e19d8281dfa2d4e522272cc7894fa5be68b3da6b
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-23 11:58:34 +0530

    Routing added to App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home.jsx";
+import Collection from "./pages/Collection.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Product from "./pages/Product.jsx";
+import Cart from "./pages/Cart.jsx";
+import Login from "./pages/Login.jsx";
+import PlaceOrder from "./pages/PlaceOrder.jsx";
+import Orders from "./pages/Orders.jsx";
 
 function App() {
   return (
-    <div className="p-6 bg-blue-500 text-white">
-      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
-      <p className="mt-4">This is a simple React app with Tailwind CSS.</p>
+    <div className="p-6 bg-blue-500 text-white sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
     </div>
   );
 }
