2355dde629436d2b0c210cc233f93c72d4acd886
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-24 15:47:20 +0530

    Defining a React Context to manage shared state across components in our application.

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,25 @@
+import {createContext} from 'react';
+import {products} from '../frontend_assets/assets';
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = () =>{
+
+    const currency = '₹'; //Unicode representation of rupee symbol is [const currency = '\u20B9'];
+    const delivery_fee = 30;
+
+    const value = {
+        products,
+        currency,
+        delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+
+}
+
+export default ShopContextProvider;
