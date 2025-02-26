5975aebe1417a2c709e6fb3422081b95fd8f68f7
sourabhchalke <sourabhchalke2157@gmail.com>
2025-02-26 12:08:14 +0530

    Fetching the product based on ID[comes from params] in Product pages

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
 
 function Product() {
-  return (
+
+  const {productId}=useParams();
+
+  const {products}=useContext(ShopContext);
+  const [productData,setProductData]=useState([]);
+  const [image,setImage]=useState("");
+
+  const fetchProductData = async()=>{
+
+    products.map((item)=>{
+      if(item._id === productId){
+        setProductData(item);
+        console.log(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    })
+
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  },[productId]);
+ 
+  return productData ? (
     <div>
       
     </div>
-  )
+  ): <div className='opacity-0'></div>
 }
 
 export default Product;
