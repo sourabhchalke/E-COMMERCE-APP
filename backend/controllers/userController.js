e4c37aa1804f3a96f063411ba23e31d31ace3c0a
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-04 14:57:10 +0530

    User[login,registration,admin-login] controller added to userController file

---
+++ backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+// Route for user login
+const loginUser = async(req,res)=>{
+
+}
+
+// Route for user register
+const registerUser = async(req,res)=>{
+    res.json({msg:"API Working"});
+}
+
+// Route for Admin Login
+const adminLogin = async()=>{
+    
+}
+
+export {loginUser,registerUser,adminLogin}
