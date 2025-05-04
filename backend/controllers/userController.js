844c46e68eb85b52bc366a737e90b31bab2c8efa
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-04 16:04:33 +0530

    Creating JsonWebToken after new user register successful for Authentication purpose

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,12 @@
 import userModel from '../models/userModel.js';
 import validator from 'validator';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// Create token using user id
+const createToken = (id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET);
+}
 
 // Route for user login
 const loginUser = async(req,res)=>{
@@ -43,10 +49,15 @@ const registerUser = async(req,res)=>{
 
         const user = await newUser.save();
         
+        //Creating JWS Token
+        const token = createToken(user._id);
+
+        res.json({success:true,token});
 
 
     }catch(error){
         console.log(error);
+        res.json({success:false,message:error.message});
     }
 }
 
