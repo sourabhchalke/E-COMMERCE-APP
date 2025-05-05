4e8caeafe8f54e0ad1ee1a348f680ad5c95fff8b
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-05 13:14:59 +0530

    product controller and product route structure created and used it in server file

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+
+// Function for adding product
+const addProduct = async(req,res)=>{
+
+}
+
+// Function for list product
+const listProducts = async(req,res)=>{
+    
+}
+
+// Function for removing product
+const removeProduct = async(req,res)=>{
+    
+}
+
+
+// Function for single product info
+const singleProduct = async(req,res)=>{
+    
+}
+
+export {addProduct,listProducts,removeProduct,singleProduct};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 
 //App Config
 const app = express();
@@ -16,8 +17,8 @@ app.use(express.json());
 app.use(cors());
 
 //API Endpoints
-
 app.use('/api/user',userRouter);
+app.use('/api/product',productRouter);
 
 app.get('/',(req,res)=>{
     res.send("API Working");
