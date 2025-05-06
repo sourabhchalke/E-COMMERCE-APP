9a9af6ee23ef04c7ecd97ddf0bce94f8e11669c1
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-06 12:42:52 +0530

    remove product(by using product id) function is created in product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -77,6 +77,16 @@ const listProducts = async (req, res) => {
 // Function for removing product
 const removeProduct = async (req, res) => {
 
+  try {
+
+     await productModel.findByIdAndDelete(req.body.id);
+     res.json({success:true,message:"Product Removed"});
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+
 }
 
 
