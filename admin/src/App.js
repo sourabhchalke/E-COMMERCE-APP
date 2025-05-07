b502589eb92cc7d8ac54045a6854f39c5a60595d
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-07 14:54:21 +0530

    Routing added to App.js file for add,list nad order components

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar'
+import { Routes,Route } from 'react-router-dom';
+import Add from './pages/Add';
+import List from './pages/List';
+import Orders from './pages/Orders';
 
 function App() {
   return (
@@ -11,6 +15,13 @@ function App() {
         <hr />
         <div className="flex w-full">
           <Sidebar/>
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+            <Routes>
+              <Route path='/add' element={<Add/>}/>
+              <Route path='/list' element={ <List/> }/>
+              <Route path='/orders' element={<Orders/>}/>
+            </Routes>
+          </div>
         </div>
       </>
     </div>
