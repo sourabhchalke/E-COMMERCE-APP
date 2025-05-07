8e3b38b38cf53ba2c8fb98360d8c8fbb283e40e8
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-07 14:40:36 +0530

    Sidebar added which contains Add,List and Orders Navlinks

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {assets} from '../admin_assets/assets';
 
 const Navbar = () => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-center'>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
       <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
