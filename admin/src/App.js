b6412231601a01eea6bf32ec27f15e9b37085c75
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-07 11:54:08 +0530

    Updated admin app with old tailwind version

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello
+      <div className="text-red-500 mt-28">Hello</div>
+      <div>Hello</div>
     </div>
   );
 }
