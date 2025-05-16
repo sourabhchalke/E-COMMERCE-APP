e868d8bb3b79e800201fec81b93ed2227a663b61
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-16 15:45:12 +0530

    Adding order features - orderController,orderRoute and orderModel created

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,21 @@
+
+
+// Placing orders using Cash on Delivery Method
+const placeOrder = async(req,res)=>{}
+
+// Placing orders using Stripe Method
+const placeOrderStripe = async(req,res)=>{}
+
+// Placing orders using Razorpay Method
+const placeOrderRazorpay = async(req,res)=>{}
+
+// All Orders data for Admin Panel
+const allOrders = async(req,res)=>{}
+
+// User Order data for Frontend
+const userOrders = async(req,res)=>{}
+
+// Update Order status from Admin Panel
+const updateStatus = async(req,res)=>{}
+
+export {placeOrder,placeOrderStripe,placeOrderRazorpay,allOrders,userOrders,updateStatus};
