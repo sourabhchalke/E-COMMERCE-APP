34b33bdf776f6b0abd54f3760cefda70f4086ee9
sourabhchalke <sourabhchalke2157@gmail.com>
2025-05-17 17:49:48 +0530

    Stripe package installed and placeOrderStripe function is created in orderController for controlling payment with cards

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,14 @@
 import orderModel from '../models/orderModel.js';
 import userModel from '../models/userModel.js';
+import Stripe from 'stripe';
 
+// Gateway Initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+console.log("Stripe : ",stripe);
+
+// Global Variables
+const currency = 'inr';
+const deliveryCharge = 30;
 
 // Placing orders using Cash on Delivery Method
 const placeOrder = async(req,res)=>{
@@ -34,7 +42,64 @@ const placeOrder = async(req,res)=>{
 }
 
 // Placing orders using Stripe Method
-const placeOrderStripe = async(req,res)=>{}
+const placeOrderStripe = async(req,res)=>{
+    try {
+        
+        const {userId,items,amount,address}=req.body;
+
+        const {origin} = req.headers;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"Stripe",
+            payment:false,
+            date:Date.now(),
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+
+        const line_items = items.map((item)=>({
+            price_data:{
+                currency:currency,
+                product_data:{
+                    name:item.name
+                },
+                unit_amount:item.price *100
+            },
+            quantity:item.quantity
+        }))
+
+        line_items.push({
+             price_data:{
+                currency:currency,
+                product_data:{
+                    name:"Delivery Charges"
+                },
+                unit_amount:deliveryCharge *100
+            },
+            quantity:1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url:`${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode:'payment',
+            
+        })
+
+        res.json({success:true,session_url:session.url});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message});
+    }
+}
 
 // Placing orders using Razorpay Method
 const placeOrderRazorpay = async(req,res)=>{}
